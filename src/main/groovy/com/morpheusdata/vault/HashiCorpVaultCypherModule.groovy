cd11df50552d1a3f44598c7bc0f384a5c6a345d8
Chris Taylor <chrispdtaylor@sky.com>
2022-06-24 09:39:53 +0100

    Cleanup and starting Version 1.2

--- src/main/groovy/com/morpheusdata/vault/HashiCorpVaultCypherModule.groovy
+++ src/main/groovy/com/morpheusdata/vault/HashiCorpVaultCypherModule.groovy
@@ -1,113 +1,104 @@
 package com.morpheusdata.vault
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.morpheusdata.cypher.Cypher;
-import com.morpheusdata.cypher.CypherModule;
-import com.morpheusdata.cypher.CypherObject;
-import com.morpheusdata.cypher.util.RestApiUtil;
-import com.morpheusdata.cypher.util.ServiceResponse;
-import com.morpheusdata.vault.util.*;
-import groovy.util.logging.Slf4j;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import static com.fasterxml.jackson.databind.type.LogicalType.Map;
-import com.morpheusdata.core.MorpheusContext;
-import groovy.json.JsonOutput
+import com.morpheusdata.cypher.Cypher
+import com.morpheusdata.cypher.CypherModule
+import com.morpheusdata.cypher.CypherObject
+import com.morpheusdata.vault.util.*
+import groovy.util.logging.Slf4j
+import com.morpheusdata.core.MorpheusContext
 import groovy.json.JsonSlurper
 import com.morpheusdata.core.Plugin
 
 @Slf4j
 class HashiCorpVaultCypherModule implements CypherModule {
 
-  Cypher cypher;
-  private static final String KV1 = "KV1";
-  private static final String KV2 = "KV2";
-  private static final String DEFAULT_ENGINE = KV2; //legacy as the native Morpheus module only supported KV2
+  Cypher cypher
+  private static final String KV1 = "KV1"
+  private static final String KV2 = "KV2"
+  private static final String DEFAULT_ENGINE = KV2 //legacy as the native Morpheus module only supported KV2
   private static final HashMap<String, AbstractVaultEngine> SUPPORTED_ENGINES = new HashMap<String, AbstractVaultEngine>(){{
-      put(KV1, new Kv1VaultEngine());
-      put(KV2, new Kv2VaultEngine());
-  }};
-  MorpheusContext morpheusContext;
-  Plugin plugin;
+      put(KV1, new Kv1VaultEngine())
+      put(KV2, new Kv2VaultEngine())
+  }}
+  MorpheusContext morpheusContext
+  Plugin plugin
   
   public void setMorpheusContext(MorpheusContext morpheusContext) {
-    this.morpheusContext = morpheusContext;
+    this.morpheusContext = morpheusContext
   }
   
   public void setPlugin(Plugin plugin) {
-    this.plugin = plugin;
+    this.plugin = plugin
   }
   
   @Override
   public void setCypher(Cypher cypher) {
-      this.cypher = cypher;
+      this.cypher = cypher
   }
   
   @Override
   public CypherObject write(String relativeKey, String path, String value, Long leaseTimeout, String leaseObjectRef, String createdBy) {
     if(value != null && value.length() > 0) {
-      String key = relativeKey;
-      String vaultPath = relativeKey;
+      String key = relativeKey
+      String vaultPath = relativeKey
       if(path != null) {
-        key = path + "/" + key;
+        key = path + "/" + key
       }
       if(relativeKey.startsWith("config/")) {
-        return new CypherObject(key,value,0l, leaseObjectRef, createdBy);
+        return new CypherObject(key,value,0l, leaseObjectRef, createdBy)
       } else {
-        String vaultUrl = this.getVaultUrl();
-        String vaultToken = this.getVaultToken();
+        String vaultUrl = this.getVaultUrl()
+        String vaultToken = this.getVaultToken()
         
-        AbstractVaultEngine vaultApiEngine = this.getVaultEngine(relativeKey);
+        AbstractVaultEngine vaultApiEngine = this.getVaultEngine(relativeKey)
         if (vaultApiEngine == null) {
-          vaultApiEngine = SUPPORTED_ENGINES.get(DEFAULT_ENGINE);
+          vaultApiEngine = SUPPORTED_ENGINES.get(DEFAULT_ENGINE)
         } else {
           vaultPath = this.getVaultPath(relativeKey) ?: relativeKey
         }
-        Boolean success = vaultApiEngine.save(vaultPath, value, vaultUrl, vaultToken);
+        Boolean success = vaultApiEngine.save(vaultPath, value, vaultUrl, vaultToken)
         if (success) {
-          return new CypherObject(key,value,leaseTimeout, leaseObjectRef, createdBy);
+          return new CypherObject(key,value,leaseTimeout, leaseObjectRef, createdBy)
         } else {
           return null
         }
       }
     } else {
-        return null; 
+        return null
     }
   }
 
   @Override
   public CypherObject read(String relativeKey, String path, Long leaseTimeout, String leaseObjectRef, String createdBy) {
-    String key = relativeKey;
-    String value;
-    String vaultPath = relativeKey;
+    String key = relativeKey
+    String value
+    String vaultPath = relativeKey
     if(path != null) {
-      key = path + "/" + key;
+      key = path + "/" + key
     }
     if(relativeKey.startsWith("config/")) {
-      return null;
+      return null
     } else {
-      String vaultUrl = this.getVaultUrl();
-      String vaultToken = this.getVaultToken();
+      String vaultUrl = this.getVaultUrl()
+      String vaultToken = this.getVaultToken()
 
-      AbstractVaultEngine vaultApiEngine = this.getVaultEngine(relativeKey);
+      AbstractVaultEngine vaultApiEngine = this.getVaultEngine(relativeKey)
       if (vaultApiEngine == null) {
-        vaultApiEngine = SUPPORTED_ENGINES.get(DEFAULT_ENGINE);
+        vaultApiEngine = SUPPORTED_ENGINES.get(DEFAULT_ENGINE)
       } else {
         vaultPath = this.getVaultPath(relativeKey) ?: relativeKey
       }
       
-      value = vaultApiEngine.read(vaultPath, vaultUrl, vaultToken);
+      value = vaultApiEngine.read(vaultPath, vaultUrl, vaultToken)
       
       try {
-        CypherObject vaultResult = new CypherObject(key,value,leaseTimeout,leaseObjectRef, createdBy);
-        vaultResult.shouldPersist = false;
-        return vaultResult;
+        CypherObject vaultResult = new CypherObject(key,value,leaseTimeout,leaseObjectRef, createdBy)
+        vaultResult.shouldPersist = false
+        return vaultResult
 
       } catch(Exception ex) {
-        ex.printStackTrace();
-        return null;
+        ex.printStackTrace()
+        return null
       }
     }
   }
@@ -115,54 +106,54 @@ class HashiCorpVaultCypherModule implements CypherModule {
   @Override
   public boolean delete(String relativeKey, String path, CypherObject object) {
     if(relativeKey.startsWith("config/")) {
-      return true;
+      return true
     } else {
-      String vaultPath = relativeKey;
-      String vaultUrl = this.getVaultUrl();
-      String vaultToken = this.getVaultToken();
+      String vaultPath = relativeKey
+      String vaultUrl = this.getVaultUrl()
+      String vaultToken = this.getVaultToken()
       
-      AbstractVaultEngine vaultApiEngine = this.getVaultEngine(relativeKey);
+      AbstractVaultEngine vaultApiEngine = this.getVaultEngine(relativeKey)
       if (vaultApiEngine == null) {
-        vaultApiEngine = SUPPORTED_ENGINES.get(DEFAULT_ENGINE);
+        vaultApiEngine = SUPPORTED_ENGINES.get(DEFAULT_ENGINE)
       } else {
         vaultPath = this.getVaultPath(relativeKey) ?: relativeKey
       }
 
-      Boolean success = vaultApiEngine.delete(vaultPath, vaultUrl, vaultToken);
+      Boolean success = vaultApiEngine.delete(vaultPath, vaultUrl, vaultToken)
       
-      return success;
+      return success
     }
   }
 
   @Override
   public String getUsage() {
-    StringBuilder usage = new StringBuilder();
+    StringBuilder usage = new StringBuilder()
 
-    usage.append("This allows secret data to be fetched from a HashiCorp Vault integration. This can be configured in the Plugin settings");
+    usage.append("This allows secret data to be fetched to/from a HashiCorp Vault server. This can be configured in the Plugin settings.")
 
-    return usage.toString();
+    return usage.toString()
   }
 
   @Override
   public String getHTMLUsage() {
-    return null;
+    return null
   }
 
   private String getVaultUrl() {
-    def settings = this.getSettings();
+    def settings = this.getSettings()
     if (settings.hashicorpVaultPluginUrl) {
-      return settings.hashicorpVaultPluginUrl;
+      return settings.hashicorpVaultPluginUrl
     } else {
-      return this.cypher.read("hashicorpVault/config/url").value;
+      return this.cypher.read("hashicorpVault/config/url").value
     }
   }
 
   private String getVaultToken() {
-    def settings = this.getSettings();
+    def settings = this.getSettings()
     if (settings.hashicorpVaultPluginUrl) {
-      return settings.hashicorpVaultPluginToken;
+      return settings.hashicorpVaultPluginToken
     } else {
-      return this.cypher.read("hashicorpVault/config/token").value;
+      return this.cypher.read("hashicorpVault/config/token").value
     }
   }
   
@@ -184,26 +175,26 @@ class HashiCorpVaultCypherModule implements CypherModule {
   }
   
   private String getVaultPath(String relativePath) {
-    String rtn = null;
+    String rtn = null
     if (relativePath != null) {
-      String[] splitPath = relativePath.split('/', 2);
+      String[] splitPath = relativePath.split('/', 2)
       if (splitPath.length > 1) {
-        rtn = splitPath[1];
+        rtn = splitPath[1]
       }
     }
-    return rtn;
+    return rtn
   }
 
   private AbstractVaultEngine getVaultEngine(String relativePath) {
-    AbstractVaultEngine rtn = null;
+    AbstractVaultEngine rtn = null
     if (relativePath != null) {
-      String[] splitPath = relativePath.split('/');
+      String[] splitPath = relativePath.split('/')
       if (splitPath.length > 0) {
-        String engine = splitPath[0].toUpperCase();
-        rtn = SUPPORTED_ENGINES.get(engine);
+        String engine = splitPath[0].toUpperCase()
+        rtn = SUPPORTED_ENGINES.get(engine)
       }
     }
-    return rtn;
+    return rtn
   }
 
 }
--- src/main/groovy/com/morpheusdata/vault/util/Kv2VaultEngine.groovy
+++ src/main/groovy/com/morpheusdata/vault/util/Kv2VaultEngine.groovy
@@ -1,41 +1,37 @@
-package com.morpheusdata.vault.util;
-import com.morpheusdata.cypher.util.ServiceResponse;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import static com.fasterxml.jackson.databind.type.LogicalType.Map;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
+package com.morpheusdata.vault.util
+import com.morpheusdata.cypher.util.ServiceResponse
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.ObjectMapper
 
 class Kv2VaultEngine extends AbstractVaultEngine {
 
   public boolean delete(String vaultPath, String vaultUrl, String vaultToken) {
-    this._delete(vaultPath, vaultUrl, vaultToken);
-    return true;
+    this._delete(vaultPath, vaultUrl, vaultToken)
+    return true
   }
 
   public String read(String vaultPath, String vaultUrl, String vaultToken) {
-    ServiceResponse resp = this._read(vaultPath, vaultUrl, vaultToken);
+    ServiceResponse resp = this._read(vaultPath, vaultUrl, vaultToken)
     if(resp.getSuccess()) {
-        ObjectMapper mapper = new ObjectMapper();
+        ObjectMapper mapper = new ObjectMapper()
         TypeReference<HashMap<String,Object>> typeRef
-                = new TypeReference<HashMap<String,Object>>() {};
-        HashMap<String,Object> responseJson = mapper.readValue(resp.getContent(), typeRef);
-        Map<String,Object> dataMap = (java.util.Map<String,Object>)(((java.util.Map<String,Object>)(responseJson.get("data"))).get("data"));
-        String vaultResult = mapper.writeValueAsString(dataMap);
-        return vaultResult;
+                = new TypeReference<HashMap<String,Object>>() {}
+        HashMap<String,Object> responseJson = mapper.readValue(resp.getContent(), typeRef)
+        Map<String,Object> dataMap = (java.util.Map<String,Object>)(((java.util.Map<String,Object>)(responseJson.get("data"))).get("data"))
+        String vaultResult = mapper.writeValueAsString(dataMap)
+        return vaultResult
     } else {
-        return null;
+        return null
     }
   }
 
   public boolean save(String vaultPath, String value, String vaultUrl, String vaultToken) {
-    String body = "{\"data\": " + value + "}";
+    String body = "{\"data\": " + value + "}"
     ServiceResponse resp = this._save(vaultPath, body, vaultUrl, vaultToken)
     if(resp.getSuccess()) {
-        return true;
+        return true
     } else {
-        return false;
+        return false
     }
   }
   
